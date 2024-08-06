75596a060130943b355cb6344b017245ba0948d2
nrak126 <daichidaichi126@gmail.com>
2024-08-06 12:53:06 +0900

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import './styles/Header.css'
+
+export function Header(){
+    return(
+        <>
+        <header>
+            <h1>portfolio</h1>
+            <nav>
+                <ul>
+                    <li>aboutme</li>
+                    <li>skills</li>
+                    <li>dddddd</li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
