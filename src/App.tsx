a7ae4b938304e98d92e66730ff723df2cc1eb15a
nrak126 <daichidaichi126@gmail.com>
2025-02-02 02:58:42 +0900

    共通スタイルの再設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { About } from "./components/About";
 import { Skills } from "./components/Skills";
+import "./components/styles/styles.css";
 
 function App() {
   return (
